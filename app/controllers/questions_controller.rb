2e30ff1e63ef2aaebe04b79dda31d7bfee4ecfa9
Hugo Meibom <219575360+hugomeibom@users.noreply.github.com>
2025-08-14 16:19:43 +0200

    Stupid coach rails finished without tests and less css

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+
+    @answer = case @question
+              when "I am going to work"
+                "Great!"
+              when /\?\z/   # ends with '?'
+                "Silly question, get dressed and go to work!"
+              else
+                "I don't care, get dressed and go to work!"
+              end
+  end
+end
